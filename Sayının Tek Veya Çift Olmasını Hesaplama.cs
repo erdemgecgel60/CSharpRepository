a1481edc0905d49f058c8b153a7bb496fe431cae
erdemgecgel60 <erdemgecgel345@gmail.com>
2025-05-16 09:34:47 +0300

    Create Sayının Tek Veya Çift Olmasını Hesaplama.cs

---
+++ Sayının Tek Veya Çift Olmasını Hesaplama.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sayi;
+
+            Console.WriteLine("Sayi : ");
+            
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0) {
+
+                Console.WriteLine("Sayi Cifttir");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Sayi Tektir");
+            }
+
+
+
+        }
+    }
+}
